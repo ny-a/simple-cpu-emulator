f5f254a6ae30cdbed001c9cc603403e3e3279498
ny-a <58758248+ny-a@users.noreply.github.com>
2021-09-27 12:21:32 +0900

    Add AppBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AppBar, Toolbar, Typography } from '@mui/material'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<div>
+    <AppBar position="static">
+      <Toolbar variant="dense">
+        <Typography variant="h6" color="inherit" component="div">
+          SIMPLE Emulator
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Component {...pageProps} />
+  </div>
+  )
 }
 export default MyApp
