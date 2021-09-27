a3f70652590c0ccdd86f33fab3c762ef5b6d34c2
ny-a <58758248+ny-a@users.noreply.github.com>
2021-09-28 02:00:55 +0900

    Updates

---
+++ _next/static/1Ot-d5G5ODC6fLmnl9YdH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/569-9ca52fe9f01ef7f502c9.js","static/chunks/pages/index-154eb838367915587923.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
